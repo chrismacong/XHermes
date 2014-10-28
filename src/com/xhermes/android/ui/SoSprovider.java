70e405728e24afa5758185b4d1187fb4b69f5126
chrismacong <chrismacong@163.com>
2014-10-28 08:52:49 +0800

    14.10.27部分修改

---
+++ src/com/xhermes/android/ui/SoSprovider.java
@@ -0,0 +1,20 @@
+package com.xhermes.android.ui;
+
+import android.content.Context;
+import android.view.View;
+
+import com.actionbarsherlock.view.ActionProvider;
+
+public class SoSprovider extends ActionProvider{
+
+	public SoSprovider(Context context) {
+		super(context);
+	}
+
+	@Override
+	public View onCreateActionView() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
